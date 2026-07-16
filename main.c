c73e478105946527a4e4d993c7a8b46a737272b9
dmarshaq <mrshovdaniil@gmail.com>
2026-07-16 02:00:42 +0000

    Standard C attributes parsing and support.

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-// int bar(int x) {
-//     return x;
-// }
-// 
-// int foo() {
-//     return bar(8);
-// }
+#define CUSTOM_MACRO " what? "
 
+[[cn::hello("info!!!"CUSTOM_MACRO"other_str")]]
+int bar(int x) {
+    return x;
+}
+
+int foo() {
+    return bar(8);
+}
+
+int f __attribute__(());
+
+[[hello("meta_main")]]
 int main() {
     printf("Hello world!\n");
-    // foo();
+
+    foo();
     return 0;
-}
+} 
 
